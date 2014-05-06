4a8d0930a0bac6b5ad78b7f3e8577a16b82679ba
James Higgs <james7780@yahoo.com>
2014-05-06 21:14:08 +0200

    blarg

--- Program.cs
+++ Program.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿// Pacman AI
+// - Game theory AI to calculate next move for pacman for a given maze state
+// Rules:
+// - Pacman may move in any direction that has a space or a pill
+// - Pacman must move in a path that maximises points (pills eaten)
+// - Pacman must avoid or eat the enemy pacman object
+//
+// TODO : Also make a "game runner" app that runs 2 AI's against each other
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
